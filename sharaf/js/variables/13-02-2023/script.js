7c385b5fb5aee708003ed71973e1c96a982ab4eb
mohamed sharaf <sazxsazx987987@gmail.com>
2023-02-13 19:53:30 +0200

    function13-02-2023

---
+++ sharaf/js/variables/13-02-2023/script.js
@@ -0,0 +1,52 @@
+function FizzBuzz() 
+{
+    if (!Number.isNaN(EnteredNumber))
+        {
+            for (let i = 0; i <= num; i++) {
+
+                if ( i % 3 == 0 && i % 5 == 0 )
+                {
+                    document.write("<h2>Fizz Buzz</h2>")
+                }
+                else if (i % 3 == 0)
+                {
+                    document.write("<h2>Fizz</h2>")
+                }
+                else if (i % 5 == 0)
+                {
+                    document.write("<h2>Buzz</h2>")
+                }
+                else
+                {
+                    document.write("<h2>"+i+"</h2>")
+                }
+            }
+        }
+        else
+        {
+            document.write("<h2> The Number You Entered Is Incorrect </h2>")
+        }
+}
+
+
+
+
+function PrintOutTriangle_2()
+{
+    if (!Number.isNaN(EnteredNumber)){
+         
+        for (let i = 1; i <= num; i++) 
+        {
+            for (let j = 1; j <= i; j++)
+            {
+               document.write("<span style='color:red'> " + j + " </span>")
+            }
+            document.write("<br>")
+        }
+    }
+    else
+    {
+        document.write("<h2> The Number You Entered Is Incorrect </h2>")
+    }
+
+}
