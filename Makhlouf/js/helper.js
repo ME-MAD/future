9a566018dd90ef778bb4cf4a9535971a8b8dbcb4
mohammedmakhlouf78 <mohammedmakhlouf78@gmail.com>
2023-02-15 22:53:08 +0200

    updates

--- Makhlouf/js/helper.js
+++ Makhlouf/js/helper.js
@@ -3,3 +3,32 @@ function getSumOfTwoNumbers(num1, num2)
     let result = num1 + num2;
     return result;
 }
+
+function pow(n,po) 
+{
+    let result = 1;
+    
+    for(let i = 1; i <= abs(po); i++)
+    {
+        result *= n;
+    }
+
+    if(po < 0)
+    {
+        return (1 / result);
+    }
+
+    return result;
+}
+
+function abs(n) 
+{
+    if(n >= 0)
+    {
+        return n;
+    }
+    else
+    {
+        return (n * -1)
+    }
+}
