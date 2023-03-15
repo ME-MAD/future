8c3414e3eef0ef9b4c80d4a7ce86618daef0b16b
mohammedmakhlouf78 <mohammedmakhlouf78@gmail.com>
2023-03-15 23:31:52 +0200

    updates

--- Makhlouf/js/helper.js
+++ Makhlouf/js/helper.js
@@ -16,3 +16,14 @@ const pow = (n , po) => {
 }
 
 const abs = n => (n >= 0) ? n : n * -1
+
+function isPrime(num) {
+    if (num == 2 || num == 3)
+      return true;
+    if (num <= 1 || num % 2 == 0 || num % 3 == 0)
+      return false;
+    for (let i = 5; i * i <= num; i += 6)
+      if (num % i == 0 || num % (i + 2) == 0)
+        return false;
+    return true;
+}
